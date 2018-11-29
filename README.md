f08ef3968eec1d6e91c06daf3557e6c75b6f181c
Danilo Canivel <d.canivel@gmail.com>
2018-11-29 11:02:02 -0500

    fix callbackURL

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # node_react_fullstack
 Example app for React with node, oauth, mongo etc...
+
+1) npm install
+2) create a google account, go to developer console (google cloud console), api, credentials, and set a new OAuth credentials client
+3) update the autorized url for with yours at callBackURL in the index and at google credentials client
+4)
--- server/index.js
+++ server/index.js
@@ -4,16 +4,24 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const app = express();
 
-passport.use(GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callBaclURL: '/auth/google/callback'
-}), (accessToken) => {
-    console.log(accessToken);
-});
+passport.use(new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback',
 
-// app.get('/', (req, res) => {
-//     res.send({message: 'hi there, im get'});
-// });
+        }, accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
 
-app.listen(5000);
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
