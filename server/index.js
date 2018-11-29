f259781d01db0397473533711f7c0bbf01eb7046
Danilo Canivel <d.canivel@gmail.com>
2018-11-29 11:35:32 -0500

    get route for the callback oauth

--- server/index.js
+++ server/index.js
@@ -23,5 +23,11 @@ app.get(
     })
 );
 
+//check the code in the url and handle the request
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
