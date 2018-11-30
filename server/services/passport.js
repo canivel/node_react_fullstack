df7af4d2c8a6ecf0e0792f51b6899b9cd8bb79e5
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-11-29 21:53:22 -0500

    add mongo connection

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose =  require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy(
     {
@@ -9,9 +12,9 @@ passport.use(new GoogleStrategy(
         callbackURL: '/auth/google/callback',
 
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        // console.log(refreshToken);
-        // console.log(profile);
+        new User ({
+            googleId: profile.id
+        }).save();
     }
     )
 );
