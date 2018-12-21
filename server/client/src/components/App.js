8279c9c1f5dbb2ceac3d34d5a1dd90efc320f470
dcanivel-at-325274068097 <IESzg7L/tD0VMsuy/YKjuPgawtjynvmwD+rO24MtpGs=>
2018-12-20 21:47:27 -0500

    add redux and router

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-const App = () => {
+import React, { Component } from "react";
+import { Router, Route, Switch } from "react-router-dom";
+import Header from "./Header";
+import Landing from "./Landing";
+import history from "../history";
+export class App extends Component {
+  render() {
     return (
-        <div>
-            Hi There!
-        </div>
+      <div className="ui container">
+        <Router history={history}>
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/" exact component={Landing} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
     );
-};
+  }
+}
 
 export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,17 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import App from './components/App'
+import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDom.render(<App />, document.querySelector('#root'));
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
